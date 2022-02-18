b3e255fd1b5b82296260a97eee6d080d30b643d2
Chris Bartkoski <upworkbartkoski@gmail.com>
2022-02-18 12:13:03 -0600

    config var

--- index.js
+++ index.js
@@ -3,34 +3,32 @@ const axios = require("axios");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
-const uri =
-  "mongodb+srv://chrisbartkoski:snowbeauty78@apy-cluster.aom2k.mongodb.net?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 const app = express();
 const port = 3001;
 
-async function run() {
+app.get("/apy", async (req, res) => {
   try {
     await client.connect();
-    const database = client.db("sample_mflix");
-    const comments = database.collection("comments");
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { name: "Mercedes Tyler" };
-    const comment = await comments.findOne(query);
-    console.log(comment);
+
+    const database = client.db("vault");
+    const collection = database.collection("apy");
+
+    const query = { name: "Curve cvxCRV" };
+    const vault = await collection.findOne(query);
+
+    return res.json(vault);
+  } catch (err) {
+    console.log(err);
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
-}
-run().catch(console.dir);
-
-app.get("/", async (req, res) => {
-  const valuts = ["0x4560b99C904aAD03027B5178CCa81584744AC01f"];
-  axios
-    .get(process.env.YEARN_API_URL)
-    .then((r) => res.send(r.data))
-    .catch((err) => res.send(err));
+  // const valuts = ["0x4560b99C904aAD03027B5178CCa81584744AC01f"];
+  // axios
+  //   .get(process.env.YEARN_API_URL)
+  //   .then((r) => res.send(r.data))
+  //   .catch((err) => res.send(err));
 });
 
 app.listen(port, () => {
